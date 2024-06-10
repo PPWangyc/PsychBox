86ff54e4200cbe36b5dc6438838f55e88fc8fefb
PPWangyc <ppwangyc@gmail.com>
2024-06-10 13:27:13 -0700

    add flair hpc

---
+++ code/flair/hpc/preprocess.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=preproc_flair
+#SBATCH --output=preproc_flair"%j".out
+#SBATCH --error=preproc_flair."%j".err
+#SBATCH --partition=normal
+#SBATCH --time=15:10:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=64G
+#SBATCH --mail-type=ALL
+
+FLAIR_PATH=${1}
+
+module load matlab
+chmod +rx batch_preproc_structural_job.m
+matlab -nodisplay -r "flair_path='${FLAIR_PATH}'; run('batch_preproc_structural.m');exit;"
---
+++ code/flair/hpc/spm_preproc.py
@@ -0,0 +1,41 @@
+import os
+import glob
+import argparse
+
+# set up the argument parser
+parser = argparse.ArgumentParser(description='Run fmriprep on all subjects in a BIDS dataset')
+parser.add_argument('--bids_root_dir', type=str, help='The root directory of the BIDS dataset')
+args = parser.parse_args()
+
+bids_root_dir = args.bids_root_dir
+# get subject list under bids_root_dir
+subject_list = [x.split('/')[-1] for x in glob.glob(os.path.join(bids_root_dir, 'sub-*'))]
+subject_list = [x.split('-')[-1] for x in subject_list]
+print(subject_list)
+# check if bids_root_dir has derivatives folder
+if not os.path.exists(os.path.join(bids_root_dir, 'derivatives')):
+    os.mkdir(os.path.join(bids_root_dir, 'derivatives'))
+# check if bids_root_dir/derivatives has fmriprep folder
+if not os.path.exists(os.path.join(bids_root_dir, 'derivatives', 'spm')):
+    os.mkdir(os.path.join(bids_root_dir, 'derivatives', 'spm'))
+
+for subject in subject_list:
+    flair_path = os.path.join(bids_root_dir, 'sub-{}'.format(subject), 'ses-*', 'anat', 'sub-{}*FLAIR.nii.gz'.format(subject))
+    flair_files = glob.glob(flair_path)
+    assert len(flair_files) == 1, 'Incorrect number of FLAIR files found for subject {}: {}'.format(subject, flair_files)
+    session = flair_files[0].split('/')[-3]
+    # make directory for each subject in derivatives folder
+    new_flair_dir = os.path.join(bids_root_dir, 'derivatives', 'spm', 'sub-{}'.format(subject), '{}'.format(session), 'anat')
+    os.makedirs(new_flair_dir, exist_ok=True)
+    # copy the flair file to the new directory
+    os.system('cp {} {}'.format(flair_files[0], new_flair_dir))
+    # gunzip the flair file
+    os.system('gunzip {}'.format(os.path.join(new_flair_dir, flair_files[0].split('/')[-1])))
+    # get the path of the gunzipped flair file
+    new_flair_path = os.path.join(new_flair_dir, flair_files[0].split('/')[-1].replace('.gz', ''))
+    command = 'sbatch batch_preproc_structural.sh {}'.format(new_flair_path)
+    print(command)
+    # os.system(command)
+    exit()
+
+print('Done!')
