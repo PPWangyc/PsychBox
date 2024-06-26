ed6c1f4ed8328642ccadbe931608236c01b38df2
PPWangyc <ppwang@stanford.edu>
2024-06-25 22:13:18 -0700

    fix

--- code/flair/hpc/spm_preproc.py
+++ code/flair/hpc/spm_preproc.py
@@ -19,22 +19,33 @@ if not os.path.exists(os.path.join(bids_root_dir, 'derivatives')):
 if not os.path.exists(os.path.join(bids_root_dir, 'derivatives', 'spm')):
     os.mkdir(os.path.join(bids_root_dir, 'derivatives', 'spm'))
 
+fail_list = []
 for subject in subject_list:
-    flair_path = os.path.join(bids_root_dir, 'sub-{}'.format(subject), 'ses-*', 'anat', 'sub-{}*FLAIR.nii.gz'.format(subject))
-    flair_files = glob.glob(flair_path)
-    assert len(flair_files) == 1, 'Incorrect number of FLAIR files found for subject {}: {}'.format(subject, flair_files)
-    session = flair_files[0].split('/')[-3]
-    # make directory for each subject in derivatives folder
-    new_flair_dir = os.path.join(bids_root_dir, 'derivatives', 'spm', 'sub-{}'.format(subject), '{}'.format(session), 'anat')
-    os.makedirs(new_flair_dir, exist_ok=True)
-    # copy the flair file to the new directory
-    os.system('cp {} {}'.format(flair_files[0], new_flair_dir))
-    # gunzip the flair file
-    os.system('gunzip {}'.format(os.path.join(new_flair_dir, flair_files[0].split('/')[-1])))
-    # get the path of the gunzipped flair file
-    new_flair_path = os.path.join(new_flair_dir, flair_files[0].split('/')[-1].replace('.gz', ''))
-    command = 'sbatch flair_preproc.sh {}'.format(new_flair_path)
-    print(command)
-    os.system(command)
+    try:
+        flair_path = os.path.join(bids_root_dir, 'sub-{}'.format(subject), 'ses-*', 'anat', 'sub-{}*FLAIR.nii.gz'.format(subject))
+        flair_files = glob.glob(flair_path)
+        assert len(flair_files) == 1, 'Incorrect number of FLAIR files found for subject {}: {}'.format(subject, flair_files)
+        session = flair_files[0].split('/')[-3]
+        # make directory for each subject in derivatives folder
+        new_flair_dir = os.path.join(bids_root_dir, 'derivatives', 'spm', 'sub-{}'.format(subject), '{}'.format(session), 'anat')
+        os.makedirs(new_flair_dir, exist_ok=True)
+        # copy the flair file to the new directory
+        os.system('cp {} {}'.format(flair_files[0], new_flair_dir))
+        # gunzip the flair file
+        os.system('gunzip {}'.format(os.path.join(new_flair_dir, flair_files[0].split('/')[-1])))
+        # get the path of the gunzipped flair file
+        new_flair_path = os.path.join(new_flair_dir, flair_files[0].split('/')[-1].replace('.gz', ''))
+        command = 'sbatch flair_preproc.sh {}'.format(new_flair_path)
+        print(command)
+        os.system(command)
+    except Exception as e:
+        print('Error processing subject {}: {}'.format(subject, e))
+        fail_list.append(subject)
+
+if len(fail_list) > 0:
+    print('Failed to process the following subjects: {}'.format
+            (fail_list))
+else:
+    print('All subjects processed successfully!')
 
 print('Done!')
